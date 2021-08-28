1faf4d6983757c214c6c7a5bf8903d812b314732
vishwagosalia <51322930+vishwagosalia@users.noreply.github.com>
2021-08-28 15:48:14 +0530

    Update core.clj
    Made more optimised and shows output after every player's turn.

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,4 +1,4 @@
-(ns tic-tac-toe.core 
+(ns tic-tac-toe.core
   (:require [clojure.set :as set]
             [clojure.string :as str]))
 
@@ -6,6 +6,8 @@
 (def v2 (atom []))
 (def board (atom []))
 (def winner (atom ""))
+(def displayBoard (atom "| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |"))
+(def toggle (atom true))
 
 (defn checkWinner?
   "check whether these numbers are added in v1 or v2 and then
@@ -16,39 +18,40 @@
            [4 5 6] [7 8 9] [1 5 9] [3 5 7]]]
     (some (fn [i] (set/subset? (set i) (set user))) s)))
 
+
 (defn in?
   "true if coll contains elm"
   [coll elm]
   (some #(= elm %) coll))
 
+
+(defn displayMetricBoard
+  [num]
+  (if @toggle
+    (swap! displayBoard str/replace (str num) "X")
+    (swap! displayBoard str/replace (str num) "O")))
+
+
 (defn askInput
   "Add user's input to user array and adds comp number.
    And then sort the atom"
-  ([user]
-   (if (>= (count @board) 8)
-     (reset! winner "Its a tie")
-     (let [num (Integer/parseInt (read-line))]
-      ;;  VALIDATE THE NUMBER
-       (if (in? @board num)
-         (do (println "Place is already taken")
-             (askInput v1))
-         (do (swap! user conj num)
-             (reset! user (vec (sort @user)))
-             (swap! board conj num)
-             (if (checkWinner? @v1)
-               (reset! winner "v1 WINS")
-               (askInput v2 (Integer/parseInt (read-line)))))))))
   ([user num]
    (if (in? @board num)
      (do (println "Place is already taken")
-         (askInput v2 (Integer/parseInt (read-line))))
-     (do (when (>= (count @board) 8)
+         (askInput user (Integer/parseInt (read-line))))
+     (do (when (= (count @board) 8)
            (reset! winner "Its a tie"))
          (swap! user conj num)
          (reset! user (vec (sort @user)))
          (swap! board conj num)
-         (when (checkWinner? @v2)
-           (reset! winner "v2 WINS"))))))
+         (displayMetricBoard num)
+         (when (> (count @board) 3)
+           (if @toggle
+             (when (checkWinner? @user)
+               (reset! winner "v1 WINS"))
+             (when (checkWinner? @user)
+               (reset! winner "v2 WINS"))))
+         (swap! toggle not)))))
 
 
 (defn main
@@ -57,8 +60,12 @@
   (println "| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |")
   (println "--------------")
   (while (str/blank? @winner)
-    (askInput v1)
-    (println @board))
+    (println (count @board))
+    (if @toggle
+      (askInput v1 (Integer/parseInt (read-line)))
+      (askInput v2 (Integer/parseInt (read-line))))
+    (println @displayBoard)
+    (println "--------------"))
   (println @winner))
 
 (main "user 1 => X and user 2 => O")
